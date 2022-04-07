f502f649009f3777969b1f7bea7c776f0e333807
Reisuke1231 <reisuke1231@gmail.com>
2022-04-07 15:00:30 +0900

    Implement task fill_blanks

---
+++ lib/tasks/batch/fill_blanks.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# rails runner Tasks::Batch::FillBlanks.execute
+
+class Tasks::Batch::FillBlanks
+  def self.execute
+    puts "-" * 15
+    puts "| Ruby Blanks |"
+    puts "-" * 15
+    
+    blanks = ['verb', 'adjective', 'noun', 'adjective', 'noun']
+    
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    answers = blanks.map do |request|
+      article = vowels.include?(request[0]) ? 'an' : 'a'
+      print "Give me #{article} #{request}: "
+      response = gets.chomp
+    end
+    
+    text = "I decided to #{answers[0]} a #{answers[1]} #{answers[2]} for my #{answers[3]} #{answers[4]}."
+    
+    puts text
+  end
+end
